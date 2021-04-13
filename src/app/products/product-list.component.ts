0b9db48a39238e4c0cb9621210c1152238efba8e
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-04-13 22:47:36 +0600

    pipe and Data binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,7 +5,11 @@ import { Component } from "@angular/core";
   templateUrl:'./product-list.component.html'
 })
 export class ProductListComponent{
+  imageWidth:number=50;
+  imageMargin:number=2;
   pageTitle:string = "Product List";
+  showImage = false;
+  listFilter='cart';
   products:any[]=[
     {
       "productId": 1,
@@ -28,4 +32,8 @@ export class ProductListComponent{
       "imageUrl": "assets/images/garden_cart.png"
     }
   ];
+
+  toggleImage():void{
+    this.showImage=!this.showImage;
+  }
 }
