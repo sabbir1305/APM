df8e22701685862996db0e2a10c3b18d1ffd64c0
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-04-14 20:04:04 +0600

    Star Component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -59,5 +59,8 @@ export class ProductListComponent implements  OnInit{
     return this.products.filter((product:IProduct)=>
     product.productName.toLowerCase().includes(filterBy));
   }
+  onRatingClicked(message:string):void{
+    this.pageTitle ='Product List: '+message;
+  }
 
 }
